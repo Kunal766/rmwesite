2c061d6c936d3b5287853698cc2949e0dab7d6f8
kunal766 <kunalnayak766@gmail.com>
2023-12-25 12:21:58 +0530

    resposive

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,10 +7,29 @@ import Slider from 'react-slick'
 function Home() {
     const settings = {
         dots: true,
-        infinite: true,
-        speed: 300,
+        infinite: false,
+        speed: 400,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     };
     const Data = [
         {
